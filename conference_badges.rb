b6e754ff6e24f25e333d30f6f559a2a45e39d62c
Samantha Barros <samantha.barros95@gmail.com>
2020-02-22 17:51:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,7 @@ end
 
 def assign_rooms(speakers)
   holding_place = []
-    speakers.each_with_index{|speaker,room| holding_place 
+    speakers.each_with_index{|speaker,room| holding_place[speaker] = room}
+  end
   
   
