24e94a631e7696dfcbad416b792f342a090982e9
Samantha Barros <samantha.barros95@gmail.com>
2020-02-22 17:38:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  #attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badges = []
+    attendees.each{|attendees| badges << "Hello, my name is #{badges}."}
+  end
+  return badges
+end
